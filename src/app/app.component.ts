6b937dbbf5f6739ef8f7d5fc07dbf5e95be6460e
GoWorkPro <info@goworkpro.com>
2024-04-02 22:03:23 +0500

    //

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UniversityListComponent } from './university-list/university-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UniversityListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
